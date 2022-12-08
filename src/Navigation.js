6195f513de5dea6f364eea2ab02af23b1bbb2e01
BennettxD2001 <rbennettito1@gmail.com>
2022-12-07 22:52:56 -0600

    Cambios Bennett cookies

--- src/Navigation.js
+++ src/Navigation.js
@@ -21,13 +21,13 @@ const Navigation = () => {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Navbar.Brand as={Link} to="/react-example/Catalog" title="Catalogo"><h4 style={{ color: "#000000" }} >Catálogo</h4></Navbar.Brand>
                         <Navbar.Brand as={Link} to="/react-example/Contact" title="Contacto"><h4 style={{ color:  "#000000" }} >Contacto</h4></Navbar.Brand>
-                        <Navbar.Brand as={Link} to="/react-example/AdminLogin" title="Administracion"><h4 style={{ color:  "#000000" }} >Administrador</h4></Navbar.Brand>
                         <Container>
                         <Nav className="me-auto">
                             <NavDropdown title={<Navbar.Brand style={{ color:  "#000000", fontSize: 18 + "pt" }}>Perfil</Navbar.Brand>} id="nav-dropdown" >
                                 <NavDropdown.Item as={Link} to="/react-example/UserProfile" title="Perfil" style={{ color:  "#000000" }}><b>Perfil</b></NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="/react-example/Login" title="Inicio sesión" style={{ color:  "#000000" }}><b>Inicio sesión</b></NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="/react-example/Home" title="Cerrar sesión" style={{ color:  "#000000" }}><b>Cerrar sesión</b></NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/react-example/AdminLogin" title="Administracion" style={{ color:  "#000000" }}> <b>Administrador</b></NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="/react-example/PurchaseOrder" title="Orden de Compra" style={{ color:  "#000000" }}><b>Órdenes de Compra</b></NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="/react-example/ShoppingCart" title="Carrito" style={{ color:  "#000000" }}><b>🛒</b></NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="/react-example/Favorites" title="Lista de deseos" style={{ color:  "#000000" }}><b>❤</b></NavDropdown.Item>
